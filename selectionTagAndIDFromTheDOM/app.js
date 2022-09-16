e39ce16e169bd2669c9b5846ffa65fece1417e25
Your Name <karabayemre10@gmail.com>
2022-09-16 11:57:31 +0300

    selectionTagAndIDFromTheDOM eklendi

---
+++ selectionTagAndIDFromTheDOM/app.js
@@ -0,0 +1,20 @@
+let titleDOM = document.querySelector('#baslik');
+
+console.log(titleDOM.innerHTML);
+
+titleDOM.innerHTML = "Bu başlık değişti";
+
+console.log(titleDOM.innerHTML);
+
+titleDOM.classList.add('title');
+
+
+let paragrafDOM = document.querySelector('.paragraf');
+
+console.log(paragrafDOM.innerHTML);
+
+paragrafDOM.innerHTML = "Bu paragraf değişti";
+
+console.log(paragrafDOM.innerHTML);
+
+
