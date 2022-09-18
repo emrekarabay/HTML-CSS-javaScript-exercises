6ae4e12a2eed3d789e570307e77c2b44fd83b04b
Your Name <karabayemre10@gmail.com>
2022-09-18 15:24:50 +0300

    backgroundColorChange eklendi

---
+++ backgroundColorChange/script.js
@@ -0,0 +1,12 @@
+var centerButton1 = document.querySelector("#button1")
+var body = document.querySelector("#body")
+var centerButton2 = document.querySelector("#button2")
+
+centerButton1.onclick = function(){
+   body.style.background = "#142d4c";
+}
+centerButton2.onclick = function(){
+   body.style.background = "#ececec";
+}
+ 
+
